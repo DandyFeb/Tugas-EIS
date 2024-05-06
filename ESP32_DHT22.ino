da447aeeb7ea27794ea8f1d485c0aeb053734fe5
DandyFeb <161456972+DandyFeb@users.noreply.github.com>
2024-05-06 08:10:52 +0700

    Create ESP32_DHT22.ino

---
+++ ESP32_DHT22.ino
@@ -0,0 +1,41 @@
+#include <DHT.h>
+
+#define DHTPIN 5
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+const int lampu = 13;
+float hum;
+float temp;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(lampu, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  hum = dht.readHumidity();
+  temp = dht.readTemperature();
+
+  Serial.print("Humidity: ");
+  Serial.print(hum);
+  Serial.print("% ");
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print("C \n");
+
+  if (temp >= 35 || hum >= 50)
+  {
+    digitalWrite(lampu, HIGH);
+  }
+  else
+  {
+    digitalWrite(lampu, LOW);
+  }
+
+  delay(2000);
+}
