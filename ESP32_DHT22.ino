b358053eb0468283313332628ada552f07ff0ee4
DandyFeb <161456972+DandyFeb@users.noreply.github.com>
2024-05-08 11:52:33 +0700

    Update ESP32_DHT22.ino

--- ESP32_DHT22.ino
+++ ESP32_DHT22.ino
@@ -1,41 +1,70 @@
+#include <LiquidCrystal_I2C.h>
 #include <DHT.h>
+#include <ESP32Servo.h>
 
-#define DHTPIN 5
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4);
+
+#define DHTPIN 19
 #define DHTTYPE DHT22
-
 DHT dht(DHTPIN, DHTTYPE);
 
-const int lampu = 13;
-float hum;
-float temp;
+#define SERVO_PIN 15
+Servo servo;
+
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  dht.begin();
-  pinMode(lampu, OUTPUT);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+
+  servo.attach(SERVO_PIN);
+  servo.write(90);
 }
+
+int pos = 0;
+float hum;
+float temp;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  hum = dht.readHumidity();
   temp = dht.readTemperature();
+  hum = dht.readHumidity();
 
-  Serial.print("Humidity: ");
-  Serial.print(hum);
-  Serial.print("% ");
-  Serial.print("Temperature: ");
-  Serial.print(temp);
-  Serial.print("C \n");
+  lcd.setCursor(0,0);
+  lcd.print("Temperatur: ");
+  lcd.print(temp);
+  lcd.print(" C");
+  lcd.setCursor(0,1);
+  lcd.print("Status: ");
+  if (temp > 40)
+    lcd.print("Panas ");
+  else if (temp < 20)
+    lcd.print("Dingin");
+  else
+    lcd.print("Normal");
 
-  if (temp >= 35 || hum >= 50)
+  lcd.setCursor(0,2);
+  lcd.print("Kelembapan: ");
+  lcd.print(hum);
+  lcd.print("%");
+  lcd.setCursor(0,3);
+  lcd.print("Status: ");
+  if (hum > 50)
+    lcd.print("Basah ");
+  else if (hum < 30)
+    lcd.print("Kering");
+  else
+    lcd.print("Normal");
+
+  if (temp > 40 || hum > 50)
   {
-    digitalWrite(lampu, HIGH);
+    servo.write(0);
   }
   else
   {
-    digitalWrite(lampu, LOW);
+    servo.write(90);
   }
 
-  delay(2000);
+  delay(100); // this speeds up the simulation
 }
